6f6866ee01da801ce7c1bffdb4a740dfae7553a2
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-03-22 15:13:31 -0600

    fix vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly  VITE_API_URL: string,
+    readonly  VITE_SECONDS_BLOCKED_PROCESS: number,
+    readonly  VITE_MAX_NUMBER_OF_PROCESSES_TO_REQUEST: number,
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
