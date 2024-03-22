58646b95effc32443f1e1b7e40ed4225f7625f70
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-03-22 14:56:10 -0600

    adding vite-env.d.ts and type for envs files

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,10 +1,6 @@
 
-type Envs = {
-    API_URL: string,
-    SECONDS_BLOCKED_PROCESS: number
-}
 
-export const envs:Envs =  {
+export const envs =  {
     API_URL: import.meta.env.VITE_API_URL || '',
     SECONDS_BLOCKED_PROCESS: +import.meta.env.VITE_SECONDS_BLOCKED_PROCESS || 8
 }
