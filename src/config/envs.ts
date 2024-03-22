cd1f78509819d6031e13b41ee1be1567470f6841
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-03-22 15:13:44 -0600

    add VITE_MAX_NUMBER_OF_PROCESSES_TO_REQUEST

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,5 +2,6 @@
 
 export const envs =  {
     API_URL: import.meta.env.VITE_API_URL || '',
-    SECONDS_BLOCKED_PROCESS: +import.meta.env.VITE_SECONDS_BLOCKED_PROCESS || 8
+    SECONDS_BLOCKED_PROCESS: +import.meta.env.VITE_SECONDS_BLOCKED_PROCESS || 8,
+    MAX_NUMBER_OF_PROCESSES_TO_REQUEST: +import.meta.env.VITE_MAX_NUMBER_OF_PROCESSES_TO_REQUEST || 50
 }
